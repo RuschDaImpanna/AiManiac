63c198269e50d1d5f88807e525b6302a6bcf8203
Brayan <brayanprse@unisabana.edu.co>
2025-08-31 14:53:28 -0500

    Set basic player movement

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private float force = 10f;
+    private Rigidbody rb;
+    private PlayerInput playerInput;
+    private Vector2 moveInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        InputAction moveAction = playerInput.actions["Move"];
+
+        moveAction.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        moveAction.canceled += ctx => moveInput = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(moveInput.x, 0f, moveInput.y) * force);
+    }
+}
