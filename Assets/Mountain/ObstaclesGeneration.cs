92199c234c71dbd87604ad1fcfa17693c96fded9
Brayan <brayanprse@unisabana.edu.co>
2025-09-05 09:49:01 -0500

    Check if the obstacle fits within the bounds of mountain
    Check if the prefab fits within the bounds of the parent object at the given local position

--- Assets/Mountain/ObstaclesGeneration.cs
+++ Assets/Mountain/ObstaclesGeneration.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class ObstaclesGeneration : MonoBehaviour
 {
+     public GameObject obstaclePrefab;  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,9 +22,6 @@ public class ObstaclesGeneration : MonoBehaviour
             for (float z = -(transform.localScale.z) / 2f; (z + gridScale.z) <= transform.localScale.z / 2f; z += gridScale.z + gapZ)
             {
                 list.Add(new Vector3((x + gridScale.x / 2f) / transform.localScale.x, 1f, (z + gridScale.z / 2f) / transform.localScale.z));
-
-
-                Debug.Log(x + gridScale.x + " < " + transform.localScale.x / 2f);
             }
 
             gridPositions.Add(list);
@@ -36,24 +34,43 @@ public class ObstaclesGeneration : MonoBehaviour
             {
                 if (Random.Range(0, 100) < 30) // 30% chance to spawn an obstacle
                 {
-                    GameObject newObstacle = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    newObstacle.transform.SetParent(transform);
+                    bool isPosAvailable = CheckPosAvailability(obstaclePrefab, pos);
+                    if (!isPosAvailable) continue;
+
+                    GameObject newObstacle = Instantiate(obstaclePrefab);
+                    newObstacle.transform.parent = transform;
                     newObstacle.transform.localPosition = pos;
                     newObstacle.transform.localRotation = Quaternion.identity;
-                    newObstacle.transform.localScale = GetRelativeSize(Vector3.one);
                 }
             }
         }
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
     Vector3 GetRelativeSize(Vector3 size)
     {
         return new Vector3(size.x / transform.localScale.x, size.y / transform.localScale.y, size.z / transform.localScale.z);
     }
+
+    bool CheckPosAvailability(GameObject prefab, Vector3 localPos)
+    {
+        Vector3 prefabRelativeSize = GetRelativeSize(prefab.transform.localScale);
+
+        if (prefabRelativeSize.x > 1f || prefabRelativeSize.z > 1f)
+        {
+            Debug.LogWarning("Prefab size exceeds the bounds of the parent object.");
+            return false;
+        }
+
+        if (localPos.x - prefabRelativeSize.x / 2f < -0.5f || localPos.x + prefabRelativeSize.x / 2f > 0.5f ||
+            localPos.z - prefabRelativeSize.z / 2f < -0.5f || localPos.z + prefabRelativeSize.z / 2f > 0.5f)
+        {
+            Debug.LogWarning("Prefab at the given position would exceed the bounds of the parent object.");
+            return false;
+        }
+
+        return true;
+    }
+
+
 }
