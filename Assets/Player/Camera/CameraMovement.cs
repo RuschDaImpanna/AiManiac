af8ec473449485986603fa8f1475333f70aaf350
Rusch Da Impanna <ruschdaimpanna@gmail.com>
2025-09-05 22:06:45 -0500

    Input Controllers and Virtual Camera created

--- Assets/Player/Camera/CameraMovement.cs
+++ Assets/Player/Camera/CameraMovement.cs
@@ -2,15 +2,11 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    
     public float mouseSensitivity = 100f;
     public bool visibleMouse = false;
 
     float xRotation = 0f;
-
-    void Start()
-    {
-
-    }
 
     void Update()
     {
