c0fc7fa3ece61fa76e58fe6b6740ac1cc024edef
Rusch Da Impanna <ruschdaimpanna@gmail.com>
2025-09-06 16:59:54 -0500

    Danger-O-Meter almost done
    This needs to be check with the looping mountain, the guns and the little boost aceleration of the W or S

--- Assets/Player/DangerOMeter.cs
+++ Assets/Player/DangerOMeter.cs
@@ -4,7 +4,15 @@ public class DangerOMeter : MonoBehaviour
 {
     private Rigidbody rb;
     private Vector3 previousVelocity;
+    private float currentSpeed;
     private Vector3 acceleration;
+
+    [Header("Avalanche Settings")]
+    public float avalancheStart;
+    public float avalancheAcceleration;
+
+    public float avalancheSpeed;
+    private float dangerMeter;
 
     void Awake()
     {
@@ -13,13 +21,33 @@ public class DangerOMeter : MonoBehaviour
 
     void FixedUpdate()
     {
-
         Vector3 currentVelocity = rb.linearVelocity;
         acceleration = (currentVelocity - previousVelocity) / Time.fixedDeltaTime;
-
-        Debug.Log($"Velocity: {currentVelocity} (Magnitude: {currentVelocity.magnitude}) | " + $"Acceleration: {acceleration} (Magnitude: {acceleration.magnitude})");
-
+        currentSpeed = currentVelocity.magnitude;
         previousVelocity = currentVelocity;
-        
+
+        Debug.Log($"Player Speed: {currentSpeed:0.00} | Accel: {acceleration.magnitude:0.00}");
+
+        if (avalancheStart > 0f)
+        {
+            avalancheStart -= Time.deltaTime;
+        }
+        else
+        {
+            // Smoothly approach target avalanche speed
+            avalancheSpeed = Mathf.MoveTowards(avalancheSpeed, currentSpeed, avalancheAcceleration * Time.deltaTime);
+
+
+            float relativeSpeed = currentSpeed - avalancheSpeed;
+
+            // Update danger meter based on relative speed
+            dangerMeter += relativeSpeed * Time.deltaTime * 0.75f;
+            //dangerMeter = Mathf.Clamp(dangerMeter, 0f, 10f);
+
+            Debug.Log($"Avalanche Speed: {avalancheSpeed:0.00} | Danger Meter: {dangerMeter:0.00}");
+
+
+        }
     }
 }
+
