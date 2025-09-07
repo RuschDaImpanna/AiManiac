b20ae9afc9de3be71d12190a896bc338b0dde2ae
Rusch Da Impanna <ruschdaimpanna@gmail.com>
2025-09-06 19:48:17 -0500

    Danger-O-Meter deleted and camera prefabs

--- Assets/Player/DangerOMeter.cs
+++ Assets/Player/DangerOMeter.cs
@@ -7,13 +7,6 @@ public class DangerOMeter : MonoBehaviour
     private float currentSpeed;
     private Vector3 acceleration;
 
-    [Header("Avalanche Settings")]
-    public float avalancheStart;
-    public float avalancheAcceleration;
-
-    public float avalancheSpeed;
-    private float dangerMeter;
-
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -21,6 +14,7 @@ public class DangerOMeter : MonoBehaviour
 
     void FixedUpdate()
     {
+
         Vector3 currentVelocity = rb.linearVelocity;
         acceleration = (currentVelocity - previousVelocity) / Time.fixedDeltaTime;
         currentSpeed = currentVelocity.magnitude;
@@ -28,26 +22,6 @@ public class DangerOMeter : MonoBehaviour
 
         Debug.Log($"Player Speed: {currentSpeed:0.00} | Accel: {acceleration.magnitude:0.00}");
 
-        if (avalancheStart > 0f)
-        {
-            avalancheStart -= Time.deltaTime;
-        }
-        else
-        {
-            // Smoothly approach target avalanche speed
-            avalancheSpeed = Mathf.MoveTowards(avalancheSpeed, currentSpeed, avalancheAcceleration * Time.deltaTime);
-
-
-            float relativeSpeed = currentSpeed - avalancheSpeed;
-
-            // Update danger meter based on relative speed
-            dangerMeter += relativeSpeed * Time.deltaTime * 0.75f;
-            //dangerMeter = Mathf.Clamp(dangerMeter, 0f, 10f);
-
-            Debug.Log($"Avalanche Speed: {avalancheSpeed:0.00} | Danger Meter: {dangerMeter:0.00}");
-
-
-        }
     }
 }
 
