6bdc1cb6f7f12b9da2a0fa1f45cf0e2e234e6d81
Brayan <brayanprse@unisabana.edu.co>
2025-10-09 19:05:45 -0500

    Add velocity penalization when crash with bouncers

--- Assets/LateralMountainSection/PlayerBounce.cs
+++ Assets/LateralMountainSection/PlayerBounce.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class PlayerBounce : MonoBehaviour
 {
+    [SerializeField, Range(0f, 1f)]
+    private float bounceForce = 1f;
+
+    [SerializeField, Range(0f, 1f)]
+    private float speedReduction = 2/3;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -10,7 +15,11 @@ public class PlayerBounce : MonoBehaviour
             if (playerRigidbody != null)
             {
                 // Invert the player's horizontal velocity to create a bounce effect
-                playerRigidbody.linearVelocity = new Vector3(-playerRigidbody.linearVelocity.x, playerRigidbody.linearVelocity.y, playerRigidbody.linearVelocity.z);
+                playerRigidbody.linearVelocity = new Vector3(
+                    -playerRigidbody.linearVelocity.x * bounceForce, 
+                    playerRigidbody.linearVelocity.y, 
+                    playerRigidbody.linearVelocity.z * speedReduction
+                );
             }
         }
     }
