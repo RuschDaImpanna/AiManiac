e04cc7595699bfa16bca52b85bcaa4d9e6f8e2a9
Brayan <brayanprse@unisabana.edu.co>
2025-10-30 18:12:09 -0500

    Set speed lines rate over time emission base on player's speed

--- Assets/Player/SpeedLines/SpeedLinesMove.cs
+++ Assets/Player/SpeedLines/SpeedLinesMove.cs
@@ -2,20 +2,36 @@ using UnityEngine;
 
 public class SpeedLinesMove : MonoBehaviour
 {
-    private Rigidbody rbPlayer;
     private SpeedBar playerSpeedBar;
-    private float speed;
+    private ParticleSystem speedLines;
+    private ParticleSystem.EmissionModule emission;
 
+    private float xParentRotation;
+
+    [SerializeField]
+    private float intensityFactor = 1f;
     void Start()
     {
         // Find and subscribe to the SpeedBar's state change event
         playerSpeedBar = FindFirstObjectByType<SpeedBar>();
+
+        speedLines = GetComponent<ParticleSystem>();
+        emission = speedLines.emission;
+
+        xParentRotation = transform.parent.gameObject.transform.rotation.eulerAngles.x;
 
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        transform.rotation = Quaternion.Euler(
+            -xParentRotation, 
+            180f, 
+            0f
+        );
+
+        // Adjust emission rate based on speed
+        emission.rateOverTime = (playerSpeedBar.Speed * 3.6f - 100f) * intensityFactor; // Multiply by factor to tune intensity
     }
 }
