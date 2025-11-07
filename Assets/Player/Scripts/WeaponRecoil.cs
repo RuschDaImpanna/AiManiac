aecb69d2fb3305f5b309bf7e2f65c441a8f9c0a9
Brayan <brayanprse@unisabana.edu.co>
2025-11-06 19:38:21 -0500

    Add repulsion shaking to weapon recoil

--- Assets/Player/Scripts/WeaponRecoil.cs
+++ Assets/Player/Scripts/WeaponRecoil.cs
@@ -1,22 +1,35 @@
+using Unity.Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class WeaponRecoil : MonoBehaviour
 {
-    private Rigidbody playerRigidbody;
-    public Transform playerCamera;
-    public float recoilForce = 1000f;
-    public float cooldown = 1.5f;
-    private float currentCooldown = 0f;
+    [Header("Shooting")]
+    [SerializeField] private float cooldown = 1.5f;
     public float CurrentCooldown { get { return currentCooldown; } }
     private float lastShootTime = -Mathf.Infinity;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float currentCooldown = 0f;
+
+    [Header("Recoil")]
+    [SerializeField] private CinemachineImpulseSource impulseSource;
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private float repulseForce = 1000f;
+    [SerializeField] private float recoilForce = 1f;
+    [SerializeField] private Vector3 recoilDirection = new Vector3(0, 0.2f, -1f);
+    
+    private Rigidbody rb;
+    
     void Start()
     {
-        playerRigidbody = GetComponentInParent<Rigidbody>();
-        InputAction shootAction = GetComponentInParent<PlayerInput>().actions["Shoot"];
+        rb = GetComponentInParent<Rigidbody>();
 
+        if (impulseSource == null)
+        {
+            impulseSource = GetComponent<CinemachineImpulseSource>();
+        }
+
+        InputAction shootAction = GetComponentInParent<PlayerInput>().actions["Shoot"];
         shootAction.performed += ctx => 
         {
             if (ctx.control.path.Contains("leftButton"))
@@ -28,12 +41,6 @@ public class WeaponRecoil : MonoBehaviour
             }
 
         };
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void FixedUpdate()
@@ -55,8 +62,11 @@ public class WeaponRecoil : MonoBehaviour
         if (currentCooldown != 0f) return;
 
         Vector3 vector3 = playerCamera.forward;
-        Vector3 recoilDirection = - new Vector3(vector3.x, 3*vector3.y/4, vector3.z/10);
-        playerRigidbody.AddForce(recoilDirection * recoilForce, ForceMode.Impulse);
+        Vector3 recoilForceDirection = - new Vector3(vector3.x, 3*vector3.y/4, vector3.z/10);
+        rb.AddForce(recoilForceDirection * repulseForce, ForceMode.Impulse);
         lastShootTime = Time.time;
+
+        // Apply camera recoil
+        impulseSource.GenerateImpulseWithVelocity(recoilDirection.normalized * recoilForce);
     }
 }
